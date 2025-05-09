d1216b3f4d67ed7d9ac7da6ae8e7c2fdefdc5db6
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-09 11:19:01 -0300

    exemplo 5 operaçoescom string

---
+++ pythonProject/capitulo 2/exemplo5operaçoescomstrings.py
@@ -0,0 +1,10 @@
+# é proibido usar opreçoes matematicas com strings
+#ao menos em 2 casos
+
+first= 'isa'
+second = 'fiori'
+nomesobrenome = first + second # + aqui significa concatenaçao
+print (nomesobrenome)
+
+stringQualquer = "teste " * 3
+print (stringQualquer)

475512a2ce2b2526f6614ad1eed83b5649bd3a80
Cauã <silva.caua27@escola.pr.gov.br>
2025-06-27 12:01:39 -0300

    exercicios 7 e 8

--- pythonProject/capitulo 3/exercicios de exemplo/exercicio6.py
+++ pythonProject/capitulo 3/exercicios de exemplo/exercicio6.py
@@ -1,5 +1,15 @@
+def right_justify(palavra, tamanhopalavra):
+    espaço = ' '
+    resultado =  espaço * (70 - tamanhopalavra)
+    return espaço + palavra
+
+
 palavra = str(input("digite uma palavra"))
 tamanhopalavra = len(palavra)
+
+justificado = right_justify(palavra, tamanhopalavra)
+
+print(justificado)
 
 
 
---
+++ pythonProject/capitulo 3/exercicios de exemplo/exercicio8.py
@@ -0,0 +1,25 @@
+def linha_inteira ():
+    l1 = print('+ - - - - + - - - - +')
+
+
+def linha_dois ():
+    l2 = print('|         |         |')
+
+
+def desenho (l1, l2):
+    l1()
+    l2()
+    l2()
+    l2()
+    l2()
+    l1()
+    l2()
+    l2()
+    l2()
+    l2()
+    l1()
+
+desenho (linha_inteira, linha_dois)
+
+
+

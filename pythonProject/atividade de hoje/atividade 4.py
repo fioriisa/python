287b8247bb0c42898f3b5630b134327e8bb1d7ff
Andrey <Andrey.schmitz@escola.pr.gov.br>
2025-09-12 11:51:01 -0300

    atividade de hoje

--- pythonProject/atividade de hoje/atividade 4.py
+++ pythonProject/atividade de hoje/atividade 4.py
@@ -1,2 +1,5 @@
-numero = int(input("Digite um número para calcular o fatorial: "))
+def fatorial (numero):
+    print (numero, "= 5*4*3*2*1 =", numero*4*3*2*1)
+numero = 5
 
+fatorial(numero)
---
+++ pythonProject/atividade de hoje/atividade 7.py
@@ -0,0 +1,16 @@
+def apagartela (tecla2):
+    apagar = ("\n" * 1000000)
+    print (apagar)
+    print ("tudo bem?")
+
+def segundotexto():
+    tecla2 = input("aperte enter novamente")
+    apagartela(tecla2)
+
+def primeirotexto (tecla1):
+    frase1 = "olá"
+    frase2 = "seja bem-vindo"
+    segundotexto()
+tecla1 = input("aperte enter")
+
+primeirotexto()
